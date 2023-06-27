941890b27ff0f220233300633810131311e845f7
RobertooFelipe <feliperoberto092@gmail.com>
2023-06-27 14:19:50 -0300

    feat: :tada: call Header component and add properties on layout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,28 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import HeaderComponent from '@/components/Header'
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Dev Team",
-  description: "Página de serviços da Dev Team, venha desenvolver sua ideia.",
-};
+  title: 'Dev Team',
+  description: 'Página de serviços da Dev Team, venha desenvolver sua ideia.'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        // className={inter.className}
+        className="h-screen w-screen bg-gradient-to-t from-blue-400 to-blue-100 bg-no-repeat"
+      >
+        <HeaderComponent />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
