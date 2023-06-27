3e1cdf2dd211eaaecd1aef03a2276394ef263a2b
RobertooFelipe <feliperoberto092@gmail.com>
2023-06-27 14:14:08 -0300

    feat: :art: add new style on tailwind presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: '#F0F0F0',
+          100: '#E1DFE2',
+          200: '#939393',
+          300: '#6C6C6C',
+          400: '#2B2B2B'
+        },
+        blue: {
+          100: '#0F1A30',
+          200: '#0D1629',
+          300: '#090E1B',
+          400: '#070B15'
+        },
+        yellow: { 800: '#FFB22B' },
+        success: '#67A950',
+        error: '#BE3737'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
