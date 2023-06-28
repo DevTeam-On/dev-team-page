3f9e62394e89ccf955a186dbe70af8e1aad9dac9
RobertooFelipe <feliperoberto092@gmail.com>
2023-06-28 11:32:46 -0300

    feat: :sparkles: call a Container component on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
 import Image from 'next/image'
 import Logo from '@/assets/images/DevTeamLogo.png'
 import ItemsHeader from './ItemsHeader'
+import Container from '@/components/Container'
 
 export default function HeaderComponent() {
   return (
-    <nav className="fixed top-0 flex w-full items-center  justify-between border-b-2  border-yellow-800 bg-blue-200/20 px-24 py-4 backdrop-blur-sm">
-      <a href="#">
-        <Image
-          src={Logo}
-          width={130}
-          height={130}
-          alt="Logo de marca usada pela Dev Team"
-        />
-      </a>
+    <nav className="fixed top-0 w-full border-b-2 border-yellow-800 bg-blue-200/20 px-24 py-4 backdrop-blur-sm">
+      <Container>
+        <div className="flex items-center justify-between">
+          <a href="#">
+            <Image
+              src={Logo}
+              width={130}
+              height={130}
+              alt="Logo de marca usada pela Dev Team"
+            />
+          </a>
 
-      <ItemsHeader />
+          <ItemsHeader />
+        </div>
+      </Container>
     </nav>
   )
 }
