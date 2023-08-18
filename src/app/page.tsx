c4c6e6b9e3a6e6a47ad7362a9e894b28d0cd9bf0
RobertooFelipe <feliperoberto092@gmail.com>
2023-08-18 09:19:18 -0300

    feat: adjust style of Component InitialContentPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import InitialContentPage from '@/components/InitalContentPage'
 export default function Home() {
   return (
     <div>
-      <div className="pt-48">
-        <InitialContentPage />
-      </div>
+      <InitialContentPage />
     </div>
   )
 }
