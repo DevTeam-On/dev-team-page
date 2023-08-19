62daf20a1954b10a2cba92243bd70ff9204efb7e
RobertooFelipe <feliperoberto092@gmail.com>
2023-08-19 09:41:18 -0300

    feat: call OurServices Componet at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Benefits from '@/components/Benefits'
 import InitialContentPage from '@/components/InitalContentPage'
+import OurServices from '@/components/OurServices'
 
 export default function Home() {
   return (
     <>
       <InitialContentPage />
       <Benefits />
+      <OurServices />
     </>
   )
 }
