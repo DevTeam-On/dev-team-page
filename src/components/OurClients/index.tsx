15a19c20ef3c41f478ebf73e2d8537ee9bf9af4e
RobertooFelipe <feliperoberto092@gmail.com>
2023-08-20 20:59:57 -0300

    feat: create OurClients Compoent

---
+++ src/components/OurClients/index.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import ClientSoSeu from '@/assets/images/client_soseu.png'
+import FullButton from '../FullButton'
+
+export default function OurClients() {
+  return (
+    <div className="flex w-full flex-col items-center gap-24">
+      <h1 className="font-mavenPro text-2xl font-semibold text-white laptop:text-5xl">
+        Nossos Clientes
+      </h1>
+      <div className="flex flex-col items-center justify-center gap-12">
+        <Image
+          src={ClientSoSeu}
+          width={200}
+          height={200}
+          alt="Cliente plataforma Só Seu"
+        />
+        <FullButton url="https://soseu.com.br/">Visualizar</FullButton>
+      </div>
+    </div>
+  )
+}
