0920fc6a3234646fe74ad41e89681a4804ace287
RobertooFelipe <feliperoberto092@gmail.com>
2023-08-21 22:59:00 -0300

    feat: add responsiveness at OurClient Component

--- src/components/OurClients/index.tsx
+++ src/components/OurClients/index.tsx
@@ -4,15 +4,15 @@ import FullButton from '../FullButton'
 
 export default function OurClients() {
   return (
-    <div className="flex w-full flex-col items-center gap-24">
+    <div className="flex w-full flex-col items-center gap-8 laptop:gap-24">
       <h1 className="font-mavenPro text-2xl font-semibold text-white laptop:text-5xl">
         Nossos Clientes
       </h1>
       <div className="flex flex-col items-center justify-center gap-12">
         <Image
           src={ClientSoSeu}
-          width={200}
-          height={200}
+          width={150}
+          height={150}
           alt="Cliente plataforma Só Seu"
         />
         <FullButton url="https://soseu.com.br/">Visualizar</FullButton>
